e8a29123f2d18ff0ebd59fce8c88cf3a911b2044
Peter Kraft <petereliaskraft@gmail.com>
2019-07-03 11:18:50 -0700

    Update predict_customer_churn_eval.py

--- tests/test_scripts/predict_customer_churn_eval.py
+++ tests/test_scripts/predict_customer_churn_eval.py
@@ -1,12 +1,12 @@
-import pandas as pd
-from timeit import default_timer as timer
-import numpy as np
-from featuretools.primitives import make_trans_primitive
-
-from featuretools.primitives import make_agg_primitive
+import os
 
 import featuretools as ft
 import featuretools.variable_types as vtypes
+import numpy as np
+import pandas as pd
+from featuretools.primitives import make_agg_primitive
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
 
 N_PARTITIONS = 1000
 
@@ -45,7 +45,7 @@ def partition_to_entity_set(partition, cutoff_time_name='MS-31_labels.csv'):
 
     # Make sure to drop duplicates
     cutoff_times = pd.read_csv(f'{partition_dir}/{cutoff_time_name}', parse_dates=['cutoff_time'])
-    cutoff_times = cutoff_times.drop_duplicates(subset=['msno', 'cutoff_time'])
+    cutoff_times = cutoff_times.drop_duplicates(subset=['msno', 'cutoff_time'])[~cutoff_times['label'].isna()]
 
     # Create empty entityset
     es = ft.EntitySet(id='customers')
@@ -121,19 +121,80 @@ agg_primitives = ['sum', 'time_since_last', 'avg_time_between', 'all', 'mode', '
 trans_primitives = ['is_weekend', 'cum_sum', 'day', 'month', 'diff', 'time_since_previous']
 where_primitives = ['sum', 'mean', 'percent_true', 'all', 'any']
 
-start = timer()
-feature_matrix, feature_defs = ft.dfs(entityset=es, target_entity='members',
-                                      cutoff_time=cutoff_times,
-                                      agg_primitives=agg_primitives,
-                                      trans_primitives=trans_primitives,
-                                      where_primitives=where_primitives,
-                                      max_depth=2, features_only=False,
-                                      verbose=1,
-                                      n_jobs=1,
-                                      cutoff_time_in_index=True)
-end = timer()
-print(f'{round(end - start)} seconds elapsed.')
+cutoff_times = cutoff_times[:100]
 
-print(feature_defs)
+if not os.path.exists(resources_folder + "features.dfs"):
+    feature_matrix, feature_defs = ft.dfs(entityset=es, target_entity='members',
+                                          cutoff_time=cutoff_times,
+                                          agg_primitives=agg_primitives,
+                                          trans_primitives=trans_primitives,
+                                          where_primitives=where_primitives,
+                                          max_depth=1, features_only=False,
+                                          verbose=1,
+                                          n_jobs=1,
+                                          cutoff_time_in_index=False)
+    # encode categorical values
+    feature_matrix, feature_defs = ft.encode_features(feature_matrix,
+                                                      feature_defs)
 
-print(feature_matrix.head())
+    # Drop columns with missing values
+    missing_pct = feature_matrix.isnull().sum() / len(feature_matrix)
+    to_drop = list((missing_pct[missing_pct > 0.9]).index)
+    to_drop = [x for x in to_drop if x != 'days_to_churn']
+
+    # Drop highly correlated columns
+    threshold = 0.95
+
+    # Calculate correlations
+    corr_matrix = feature_matrix.corr().abs()
+
+    # Subset to the upper triangle of correlation matrix
+    upper = corr_matrix.where(
+        np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+
+    # Identify names of columns with correlation above threshold
+    to_drop = to_drop + [column for column in upper.columns if any(
+        upper[column] >= threshold)]
+
+    to_drop_index = list(map(lambda x: list(feature_matrix.columns).index(x), to_drop))
+    feature_defs = [feature_defs[i] for i in range(len(feature_defs)) if i not in to_drop_index]
+    ft.save_features(feature_defs, resources_folder + "features.dfs")
+
+feature_defs = ft.load_features(resources_folder + "features.dfs")
+
+split_date = pd.datetime(2016, 8, 1)
+cutoff_train = cutoff_times.loc[cutoff_times['cutoff_time'] < split_date].copy()
+cutoff_valid = cutoff_times.loc[cutoff_times['cutoff_time'] >= split_date].copy()
+print("%d train rows, %d test rows" % (len(cutoff_train), len(cutoff_valid)))
+
+feature_matrix_train = ft.calculate_feature_matrix(feature_defs,
+                                                   entityset=es,
+                                                   cutoff_time=cutoff_train,
+                                                   verbose=True).drop(columns=['days_to_churn', 'churn_date'])
+feature_matrix_train = feature_matrix_train.replace({np.inf: np.nan, -np.inf: np.nan}).\
+    fillna(feature_matrix_train.median())
+
+feature_matrix_valid = ft.calculate_feature_matrix(feature_defs,
+                                                   entityset=es,
+                                                   cutoff_time=cutoff_valid,
+                                                   verbose=True).drop(columns=['days_to_churn', 'churn_date'])
+feature_matrix_valid = feature_matrix_valid.replace({np.inf: np.nan, -np.inf: np.nan}).\
+    fillna(feature_matrix_valid.median())
+
+y, test_y = np.array(feature_matrix_train.pop('label')), np.array(feature_matrix_valid.pop('label'))
+
+model = RandomForestClassifier(n_estimators=100, max_depth=40,
+                               min_samples_leaf=50,
+                               n_jobs=-1, class_weight='balanced',
+                               random_state=50)
+
+
+model.fit(feature_matrix_train, y)
+
+preds = model.predict(feature_matrix_valid)
+
+acc = roc_auc_score(test_y, preds)
+
+print("AUC Score: %f" % acc)
+
+
