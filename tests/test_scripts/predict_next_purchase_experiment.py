2d1d95faad3aee22ac6ba7c45bbcf8e6afd31cba
Peter Kraft <petereliaskraft@gmail.com>
2019-07-16 14:21:45 -0700

    Update predict_next_purchase_experiment.py

--- tests/test_scripts/predict_next_purchase_experiment.py
+++ tests/test_scripts/predict_next_purchase_experiment.py
@@ -1,4 +1,5 @@
 import pickle
+import copy
 
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
@@ -13,7 +14,7 @@ data_large = "data_large"
 
 data_folder = data_large
 
-MAX_ORDERS_PER_PERSON = 30
+MAX_ORDERS_PER_PERSON = 5
 
 
 def sample_es(es):
@@ -49,10 +50,11 @@ def sample_es(es):
                              time_index="order_time")
 
     es.add_relationship(ft.Relationship(es["orders_sampled"]["order_id"], es["order_products_sampled"]["order_id"]))
-    es.normalize_entity(base_entity_id="orders_sampled", new_entity_id="users", index="user_id")
+    es.add_relationship(ft.Relationship(es["users"]["user_id"], es["orders_sampled"]["user_id"]))
 
 
 def feature_to_sample(feature, entities_map, new_metadata):
+    feature = copy.copy(feature)
     feature_id = feature.entity_id
     feature.entityset = new_metadata
     if isinstance(feature.relationship_path, list):
@@ -64,8 +66,9 @@ def feature_to_sample(feature, entities_map, new_metadata):
             relationship._child_entity_id = entities_map[relationship._child_entity_id]
     if feature_id in entities_map:
         feature.entity_id = entities_map[feature_id]
-    for base_feature in feature.base_features:
-        feature_to_sample(base_feature, entities_map, new_metadata)
+    for i, base_feature in enumerate(feature.base_features):
+        feature.base_features[i] = feature_to_sample(base_feature, entities_map, new_metadata)
+    return feature
 
 
 if __name__ == '__main__':
@@ -94,7 +97,7 @@ if __name__ == '__main__':
     y_test = label_times_test.pop("label")
 
     sample_es(es)
-    feature_to_sample(use_features[1],
+    use_features[1] = feature_to_sample(use_features[1],
                       {"orders": "orders_sampled", "order_products": "order_products_sampled"}, es.metadata)
 
     # Train model with top features.
@@ -104,9 +107,9 @@ if __name__ == '__main__':
                                                           cutoff_time=label_times_test,
                                                           chunk_size=len(label_times_test))
     top_feature_matrix_test = top_feature_matrix_test.fillna(0)
+    y_preds = model.predict(top_feature_matrix_test)
     full_time_elapsed = time.time() - full_t0
 
-    y_preds = model.predict(top_feature_matrix_test)
     score = roc_auc_score(y_test, y_preds)
 
     print("Time: %f  AUC: %f  Length %d" % (full_time_elapsed, score, len(label_times_test)))
